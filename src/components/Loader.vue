5c85af37f8a50ef531eb217d0f0f30fbbc8af2ba
adinapop <adinampop@yahoo.com>
2021-07-15 16:31:26 +0200

    Did yesterday's bonus and try a first attempt of a dynamic select genre

---
+++ src/components/Loader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="loader">
+       Loading...
+    </div>    
+</template>
+
+
+<script>
+export default {
+    name: "Loader"
+}
+</script>
+
+<style lang="scss" scoped>
+    .loader {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: #1e2d3b;
+        color: gray;
+        font-size: 48px;
+    }
+</style>
